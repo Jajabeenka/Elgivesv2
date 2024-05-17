738a55cf71d11e55e0da6860d6947d480d81122c
cess <pmariate@up.edu.ph>
2024-05-17 16:14:31 +0800

    chore : merged two branches

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,8 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
-import firebase_core
 import firebase_auth
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
